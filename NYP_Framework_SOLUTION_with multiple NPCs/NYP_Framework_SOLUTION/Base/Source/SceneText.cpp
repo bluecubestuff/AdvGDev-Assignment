59d709891264878260285bf9448e796d0e614165
Samuel <bluecubestuff@gmail.com>
2017-12-15 13:11:38 +0800

    >wall lod

--- NYP_Framework_SOLUTION_with multiple NPCs/NYP_Framework_SOLUTION/Base/Source/SceneText.cpp
+++ NYP_Framework_SOLUTION_with multiple NPCs/NYP_Framework_SOLUTION/Base/Source/SceneText.cpp
@@ -168,7 +168,9 @@ void SceneText::Init()
 	MeshBuilder::GetInstance()->GenerateOBJ("destroyedLeg", "OBJ//cube.obj")->textureID = LoadTGA("Image//boom.tga");
 
 	//random stuff mesh 
-	MeshBuilder::GetInstance()->GenerateCube("bluecube", Color(0, 0, 1), 1.0f);
+	MeshBuilder::GetInstance()->GenerateCube("greencube", Color(0, 1, 0), 1.0f);
+	MeshBuilder::GetInstance()->GenerateCube("bluecube", Color(0, 0, 1), 0.9f);
+	MeshBuilder::GetInstance()->GenerateCube("blackcube", Color(0, 0, 0), 0.8f);
 
 	// Set up the Spatial Partition and pass it to the EntityManager to manage
 	CSpatialPartition::GetInstance()->Init(100, 100, 10, 10);
@@ -184,8 +186,8 @@ void SceneText::Init()
 	GenericEntity* wall = Create::Entity("bluecube", Vector3(-10.0f, 0.0f, -20.0f));
 	wall->SetScale(Vector3(10.f,10.f,0.5f));
 	wall->SetCollider(true);
-	wall->SetAABB(Vector3(10.f, 10.f, 1.f), Vector3(-10.f, -10.f, -1.f));
-	//wall->InitLOD()
+	wall->SetAABB(Vector3(10.f, 10.f, 0.5f), Vector3(-10.f, -10.f, -0.5f));
+	wall->InitLOD("greencube", "bluecube", "blackcube");
 
 	//============================================================================
 	//GenericEntity* aCube = Create::Entity("bluecube", Vector3(-20.0f, 0.0f, -20.0f));
